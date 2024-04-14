0c677ca8613d0ffe109702f60163d5a2129b6956
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-04-14 11:49:33 -0300

    fix

--- projeto_final/settings.py
+++ projeto_final/settings.py
@@ -132,8 +132,9 @@ USE_TZ = True
 
 # Defina o diretório onde seus arquivos estáticos serão coletados
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATIC_ROOT = os.path.join('static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 MEDIA_URL = '/media/'
